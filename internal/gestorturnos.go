33b0f8af76201454b2d841dafd8efa8416514b58
Fernando Cuesta <fernandocuestab@gmail.com>
2024-12-06 13:39:58 +0100

    Carlosmapego8#9 Crea un patron singleton para GestorTurnos

--- internal/gestorturnos.go
+++ internal/gestorturnos.go
@@ -1,5 +1,20 @@
 package gestorturnos
 
+import (
+	"sync"
+)
+
 type GestorTurnos struct {
 	Auxiliares []Auxiliar
+}
+
+// Singleton de GestorTurnos
+var instance *GestorTurnos
+var once sync.Once
+
+func GetInstance() *GestorTurnos {
+	once.Do(func() {
+		instance = &GestorTurnos{}
+	})
+	return instance
 }
